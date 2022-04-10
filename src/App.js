11735096da7c86a765c0fab3ae1927923f9445ea
ujeongoh <yujoh94@gmail.com>
2022-04-10 11:52:09 +0900

    changed app text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>I was changed!</p>
+        <p>I was changed on the feature branch</p>
       </header>
     </div>
   );
